9200876b59c62c6f59966aba9507c5c255b5b910
AnilLG <anilgaikwad280894@gmail.com>
2020-01-21 06:40:04 +0530

    file modified

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/entry', methods = ['POST'])
+@app.route('/', methods = ['POST'])
 
 def index():
     return '<h1> Deployed to heroku!!!</h1>'
